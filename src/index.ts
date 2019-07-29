ea484cf2e14c58d4bb20ae500987bb160cf63deb
Geon Lee <leegun2003@gmail.com>
2019-07-30 00:04:56 +0900

    ✨ create typeorm entity

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import { GraphQLServer } from 'graphql-yoga'
-// ... or using `require()`
-// const { GraphQLServer } = require('graphql-yoga')
+import { importSchema } from 'graphql-import'
+import { resolvers } from "./resolvers";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`
+const typeDefs = importSchema('schema.graphql')
 
-const resolvers = {
-    Query: {
-        hello: (_: unknown, { name }: any) => `Hello ${name || 'World'}`,
-    },
-}
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 server.start(() => console.log('Server is running on localhost:4000'))
